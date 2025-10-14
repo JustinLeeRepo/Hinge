a810a8c9fe4135299d79ce76c3bed594d37cd033
Justin Lee <justinj.lee23@gmail.com>
2025-10-14 16:20:07 -0700

    Init

---
+++ Hinge/HingeApp.swift
@@ -0,0 +1,17 @@
+//
+//  HingeApp.swift
+//  Hinge
+//
+//  Created by Justin Lee on 10/14/25.
+//
+
+import SwiftUI
+
+@main
+struct HingeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            Text("YEAH")
+        }
+    }
+}
